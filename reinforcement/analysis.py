e0041e2357e35bb6a0d95a08143fab810fea4d0c
Jason Chen <98106167+GuanYen@users.noreply.github.com>
2024-03-16 13:46:20 -0500

    Q8

--- reinforcement/analysis.py
+++ reinforcement/analysis.py
@@ -60,9 +60,10 @@ def question3e():
     # If not possible, return 'NOT POSSIBLE'
 
 def question8():
-    answerEpsilon = None
-    answerLearningRate = None
-    return answerEpsilon, answerLearningRate
+    answerEpsilon = 0.05
+    answerAlpha = 0.2
+    answerGamma = 0.8
+    return answerEpsilon, answerAlpha, answerGamma
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
